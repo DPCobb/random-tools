d1f518ba05a743a7da8ddc2a5caab0b638cb2c57
Dan Cobb <dancobb@gmail.com>
2022-04-22 01:50:26 -0400

    expand recon script

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Collection of Random Tools
 
 suzz.sh is a simple web fuzzer that replaces suzz with an entry from a wordlist
+
+recon.sh has options to run multiple tools requires: nmap, jq, whatweb, nslookup, whois, curl, gobuster
--- recon.sh
+++ recon.sh
@@ -1,5 +1,19 @@
 #! /bin/bash
 
+reverse_who=false
+
+function gobuster_tool() {
+    printf "\n\n----- gobuster results -----\n"
+    printf "Enter Host\n"
+    read host
+    printf "Enter word list path\n"
+    read wordlist
+    res=$(gobuster -u $host -w $wordlist) 
+    printf "$res\n\n"
+    res=$(gobuster -m dns -u $host -w $wordlist) 
+    printf "$res\n\n"
+}
+
 function nmap_scan() {
     printf "\n\n----- nmap scan-----\n"
     printf "Enter IP\n"
@@ -33,6 +47,39 @@ function whatweb_scan() {
     menu
 }
 
+function whois_rep() {
+    if [ $reverse_who != true ]; then
+        printf "\n\n----- whois -----\n"
+        printf "\nEnter IP:\n"
+        read ip
+        res=$(whois $ip)
+        printf "whois result for $ip:\n"
+        printf "$res\n\n"
+        menu
+    else
+        printf "\n\n----- reverse whois -----\n"
+        printf "\nEnter IP:\n"
+        read ip
+        res=$(whois -h whois.cymru.com $ip)
+        printf "reverse whois result for $ip:\n"
+        printf "$res\n\n"
+        menu
+    fi
+}
+
+function cert_check() {
+    printf "\n\n----- cert check -----\n"
+    printf "\nEnter Host:\n"
+    read host
+    res=$(curl "https://crt.sh/?q=$host&output=json")
+    printf "cert check result for $host:\n"
+    echo $res > temp.txt
+    jq '.' temp.txt > cert.json
+    rm temp.txt
+    printf "Results sent to cert.json\n\n"
+    menu
+}
+
 function menu() {
     selection='blank'
     while [ $selection != 'quit' ]
@@ -42,6 +89,10 @@ function menu() {
 [1] Basic nmap scan of target
 [2] whatweb scan
 [3] nslookup (get IP)
+[4] whois
+[5] Reverse whois
+[6] Get certificate information
+[7] Run gobuster
 [q] Quit
 
 Enter your choice...\n"
@@ -62,6 +113,19 @@ Enter your choice...\n"
                     ;;
                 3)
                     nslookup_scan
+                    ;;
+                4)
+                    whois_rep
+                    ;;
+                5)
+                    reverse_who=true
+                    whois_rep
+                    ;;
+                6)
+                    cert_check
+                    ;;
+                7)
+                    gobuster_tool
                     ;;
             esac
         fi
